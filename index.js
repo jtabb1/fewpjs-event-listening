470f8f32804161ab624d5dac7686b78289ee4984
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-04 15:27:24 -0400

    Correct an error of omission

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
     alert('I was clicked!');
     input.addEventListener('click', clickAlert);
 }
+
+input.addEventListener('click', clickAlert);
 
 function clickAlert() {
     alert('I was clicked!');
